c43666ca3ba7eb675399c6f0259bcc733f33cd00
Aurora0087 <debrajbanshi1@gmail.com>
2026-04-06 23:01:04 +0530

    googleSiteVerification changed

--- src/config/analytics.ts
+++ src/config/analytics.ts
@@ -3,6 +3,6 @@
 // - Google Analytics Measurement ID: for Google Analytics
 // - Google Tag Manager ID: for Google Tag Manager
 
-export const googleSiteVerification = ''
+export const googleSiteVerification = 'NxVBe6xJhIvJlgJCjGhHjgZCHl2JFge3Vk8socEQiCI'
 export const googleAnalyticsMeasurementID = import.meta.env.PUBLIC_GA_TRACKING_ID // Replace with your Google Analytics Measurement ID (e.g. "G-XXXXXXXXXX")
 export const googleTagManagerID = ''
